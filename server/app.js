8c9c32dfad6f7d189596b2c8d5b3ac8cfac74042
student <student@2B8-iMac.local>
2017-04-11 09:10:50 -0400

    Worked on api links/link

--- server/app.js
+++ server/app.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
+const db = require('./models')
 
 // This address is relative to where the node process is started.
 app.use(express.static('public'))
+
+app.get('/api/links', function (req, res) {
+  db.link.findAll().then(links => {
+      res.json(links)
+  })
+})
+
+app.get('/api/link/:linkid', function (req, res) {
+  db.link.findOne({where: {uuid:req.linkid} }).then(link => {
+      res.json(link)
+  })
+})
 
 module.exports = app
