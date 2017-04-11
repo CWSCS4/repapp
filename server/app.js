c7d21ada128145721860a4ce4159802e9f09bd83
Randy <randyzhou1999@gmail.com>
2017-04-11 19:23:41 -0400

    Finished /api/link/link-id, worked on /api/link POST requests.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-const db = require('./models')
+const db = require('./database.js')
+const bodyParser = require('body-parser')
 
 // This address is relative to where the node process is started.
 app.use(express.static('public'))
+
+app.use(bodyParser.json())
 
 app.get('/api/links', function (req, res) {
   db.link.findAll().then(links => {
@@ -12,8 +15,14 @@ app.get('/api/links', function (req, res) {
 })
 
 app.get('/api/link/:linkid', function (req, res) {
-  db.link.findOne({where: {uuid:req.linkid} }).then(link => {
+  db.link.findOne({where: {uuid:req.params.linkid} }).then(link => {
       res.json(link)
+  })
+})
+
+app.post('/api/link', function (req, res) {
+  database.link.create({college:req.body.collegeName, repName:req.body.repName, tierPriority:req.body.tier, notesToCollege:req.body.repName.toCollege}).then(linkObject => {
+    res.json({uuid:linkObject.uuid})
   })
 })
 
