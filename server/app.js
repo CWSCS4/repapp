9dad3166051a308385f145d495bc83cf958880de
student <student@2B8-iMac.local>
2017-04-14 12:49:15 -0400

    Using sequelize for session store

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto')
+const {sequelize} = require('./database')
 const express = require('express')
 const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const apiRouter = require('./app-routers/api')
 
@@ -9,7 +11,8 @@ const app = express()
 app.use(session({
   secret: crypto.randomBytes(64).toString('hex'),
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: new SequelizeStore({db: sequelize})
 }))
 
 app.use('/api', apiRouter)
