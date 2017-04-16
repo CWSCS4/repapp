857e1a867a245153f7ae52afd334eebb71f73463
Randy <randyzhou1999@gmail.com>
2017-04-16 17:19:04 -0400

    Finished link creation/deletion, read-notes and upcoming visits.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const db = require('./database.js')
+const db = require('./database')
+//const restrictToLoggedIn = require('./restrict-to-logged-in')
 const bodyParser = require('body-parser')
 
 // This address is relative to where the node process is started.
@@ -10,20 +11,62 @@ app.use(bodyParser.json())
 
 app.get('/api/links', function (req, res) {
   db.link.findAll().then(links => {
-      res.json(links)
+      res.json({success: true, data: links})
+  }).catch(function (err){
+    res.json({success: false, message: err.message})
   })
 })
 
 app.get('/api/link/:linkid', function (req, res) {
   db.link.findOne({where: {uuid:req.params.linkid} }).then(link => {
-      res.json(link)
+      res.json({success: true, data: link})
+  }).catch(function (err){
+    res.json({success: false, message: err.message})
   })
 })
 
 app.post('/api/link', function (req, res) {
-  database.link.create({college:req.body.collegeName, repName:req.body.repName, tierPriority:req.body.tier, notesToCollege:req.body.repName.toCollege}).then(linkObject => {
-    res.json({uuid:linkObject.uuid})
+  db.link.create({college:req.body.collegeName, repName:req.body.repName, tierPriority:req.body.tierPriority, notesToCollege:req.body.toCollege}).then(linkObject => {
+    res.json({success: true, uuid: linkObject.uuid})
+  }).catch(function (err){
+    res.json({success: false, message: err.message})
   })
 })
+
+app.delete('/api/link/:linkid', function (req, res) {
+  db.link.destroy({where: {uuid:req.params.linkid} }).then(link => {
+      res.json({success: true})
+  }).catch(function (err){
+    res.json({success: false, message: err.message})
+  })
+})
+
+app.get('/api/read-notes/:linkid', function (req, res) {
+  db.link.update({notesFromCollegeSeen: true},{where: {uuid:req.params.linkid} }).then(link => {
+      res.json({success: true})
+  }).catch(function (err){
+    res.json({success: false, message: err.message})
+  })
+})
+
+//Assumes all completed visits have been deleted.
+app.get('/api/upcoming', function (req, res) {
+  db.link.findAll({
+    limit: 10,
+    order: '"scheduledTime" ASC'}).then(links => {
+      res.json({success: true, visits: links})
+  }).catch(function (err){
+    res.json({success: false, message: err.message})
+  })
+})
+
+
+/*app.get('/api/temp/:linkid', function (req, res) {
+  db.link.update({scheduledTime: ["2017-05-16 15:58:10.458000", "2017-05-17 15:54:10.458000"]},{where: {uuid:req.params.linkid} }).then(link => {
+      res.json({success: true})
+  }).catch(function (err){
+    res.json({success: false, message: err.message})
+  })
+})*/
 
 module.exports = app
--- server/start.js
+++ server/start.js
@@ -5,7 +5,20 @@
 
 const app = require('./app')
 const database = require('./database')
+const request = require('request')
 
 database.sequelize.sync().then(function() {
   app.listen(8000)
 })
+
+/*request.post(
+    'http://localhost:8000/api/link',
+    { json: {collegeName:"Northwestern", repName:"Unknown", tierPriority:1, toCollege:"Hello!"} },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+)*/
+
+//request.delete('http://localhost:8000/api/link/b9d64482-9350-4565-8f70-0ab62d6d918a')
