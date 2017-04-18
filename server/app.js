4deb854daae4675e4e7741e18ec5df6eab3b3bf0
student <student@2B9-iMac.local>
2017-04-18 09:10:11 -0400

    Added /api/periods and responded to comments.

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express()
 const db = require('./database')
 //const restrictToLoggedIn = require('./restrict-to-logged-in')
 const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
 
 // This address is relative to where the node process is started.
 app.use(express.static('public'))
@@ -10,7 +11,7 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 
 app.get('/api/links', function (req, res) {
-  db.link.findAll().then(links => {
+  db.link.findAll({attributes: ['college','uuid','scheduledTime','notesFromCollege','notesFromCollegeSeen','lastSignedIn']}).then(links => {
       res.json({success: true, data: links})
   }).catch(function (err){
     res.json({success: false, message: err.message})
@@ -26,7 +27,12 @@ app.get('/api/link/:linkid', function (req, res) {
 })
 
 app.post('/api/link', function (req, res) {
-  db.link.create({college:req.body.collegeName, repName:req.body.repName, tierPriority:req.body.tierPriority, notesToCollege:req.body.toCollege}).then(linkObject => {
+  db.link.create({
+  	college: req.body.collegeName,
+  	repName: req.body.repName,
+  	tierPriority: req.body.tierPriority,
+  	notesToCollege: req.body.toCollege
+  }).then(linkObject => {
     res.json({success: true, uuid: linkObject.uuid})
   }).catch(function (err){
     res.json({success: false, message: err.message})
@@ -52,6 +58,8 @@ app.get('/api/read-notes/:linkid', function (req, res) {
 //Assumes all completed visits have been deleted.
 app.get('/api/upcoming', function (req, res) {
   db.link.findAll({
+  	attributes: ['college','scheduledTime'],
+  	where: {scheduledTime:{$ne:null}},
     limit: 10,
     order: '"scheduledTime" ASC'}).then(links => {
       res.json({success: true, visits: links})
@@ -68,5 +76,13 @@ app.get('/api/upcoming', function (req, res) {
     res.json({success: false, message: err.message})
   })
 })*/
+
+app.get('/api/periods', function (req, res) {
+	db.period.findAll({attributes:['day','period','start','end']}).then(periods => {
+		res.json({success:true, data:periods})
+	}).catch(function (err){
+    res.json({success: false, message: err.message})
+  })
+})
 
 module.exports = app
--- server/start.js
+++ server/start.js
@@ -9,16 +9,21 @@ const request = require('request')
 
 database.sequelize.sync().then(function() {
   app.listen(8000)
-})
 
-/*request.post(
+  request.post(
     'http://localhost:8000/api/link',
     { json: {collegeName:"Northwestern", repName:"Unknown", tierPriority:1, toCollege:"Hello!"} },
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body)
         }
+        else {console.log(error)}
     }
-)*/
+)
+})
+
+
+
+//database.tier.create({priority:1,description:"Temp"})
 
 //request.delete('http://localhost:8000/api/link/b9d64482-9350-4565-8f70-0ab62d6d918a')
