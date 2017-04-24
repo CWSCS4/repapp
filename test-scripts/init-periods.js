eb7effe5eda2988af47e6a346374b458a557bbac
student <student@2B10-iMac.local>
2017-04-24 12:32:06 -0400

    Fixed Thursday 7th time; now deleting existing periods first

--- test-scripts/init-periods.js
+++ test-scripts/init-periods.js
@@ -5,16 +5,16 @@ function makeTime(hour, minute) {
   return new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute)
 }
 
+const MWF_PERIODS = new Map()
+  .set('1', [makeTime(8, 30), makeTime(9, 10)])
+  .set('2', [makeTime(9, 15), makeTime(9, 55)])
+  .set('3', [makeTime(10, 25), makeTime(11, 5)])
+  .set('4', [makeTime(11, 10), makeTime(11, 50)])
+  .set('5', [makeTime(11, 55), makeTime(12, 35)])
+  .set('6', [makeTime(1, 20), makeTime(2, 0)])
+  .set('7', [makeTime(2, 5), makeTime(2, 45)])
 const SCHEDULE = new Map()
-  .set('Monday', new Map()
-    .set('1', [makeTime(8, 30), makeTime(9, 10)])
-    .set('2', [makeTime(9, 15), makeTime(9, 55)])
-    .set('3', [makeTime(10, 25), makeTime(11, 5)])
-    .set('4', [makeTime(11, 10), makeTime(11, 50)])
-    .set('5', [makeTime(11, 55), makeTime(12, 35)])
-    .set('6', [makeTime(1, 20), makeTime(2, 0)])
-    .set('7', [makeTime(2, 5), makeTime(2, 45)])
-  )
+  .set('Monday', MWF_PERIODS)
   .set('Tuesday', new Map()
     .set('1', [makeTime(8, 30), makeTime(9, 10)])
     .set('2', [makeTime(9, 15), makeTime(9, 55)])
@@ -24,15 +24,7 @@ const SCHEDULE = new Map()
     .set('6', [makeTime(1, 45), makeTime(2, 25)])
     .set('7', [makeTime(2, 30), makeTime(3, 10)])
   )
-  .set('Wednesday', new Map()
-    .set('1', [makeTime(8, 30), makeTime(9, 10)])
-    .set('2', [makeTime(9, 15), makeTime(9, 55)])
-    .set('3', [makeTime(10, 25), makeTime(11, 5)])
-    .set('4', [makeTime(11, 10), makeTime(11, 50)])
-    .set('5', [makeTime(11, 55), makeTime(12, 35)])
-    .set('6', [makeTime(1, 20), makeTime(2, 0)])
-    .set('7', [makeTime(2, 5), makeTime(2, 45)])
-  )
+  .set('Wednesday', MWF_PERIODS)
   .set('Thursday', new Map()
     .set('1', [makeTime(8, 30), makeTime(9, 10)])
     .set('2', [makeTime(9, 15), makeTime(9, 55)])
@@ -40,28 +32,26 @@ const SCHEDULE = new Map()
     .set('4', [makeTime(11, 20), makeTime(12, 0)])
     .set('5', [makeTime(1, 55), makeTime(2, 35)])
     .set('6', [makeTime(2, 40), makeTime(3, 20)])
-    .set('7', [makeTime(3, 25), makeTime(4, 25)])
+    .set('7', [makeTime(3, 25), makeTime(4, 5)])
   )
-  .set('Friday', new Map()
-    .set('1', [makeTime(8, 30), makeTime(9, 10)])
-    .set('2', [makeTime(9, 15), makeTime(9, 55)])
-    .set('3', [makeTime(10, 25), makeTime(11, 5)])
-    .set('4', [makeTime(11, 10), makeTime(11, 50)])
-    .set('5', [makeTime(11, 55), makeTime(12, 35)])
-    .set('6', [makeTime(1, 20), makeTime(2, 0)])
-    .set('7', [makeTime(2, 5), makeTime(2, 45)])
-  )
+  .set('Friday', MWF_PERIODS)
 
-for (const [day, periods] of SCHEDULE) {
-  for (const [periodName, [start, end]] of periods) {
-    Period.create({
-      day,
-      period: periodName,
-      start,
-      end
-    })
-      .catch(err => {
-        throw err
-      })
-  }
-}
+Period.destroy({where: {}})
+  .then(() => {
+    for (const [day, periods] of SCHEDULE) {
+      for (const [periodName, [start, end]] of periods) {
+        Period.create({
+          day,
+          period: periodName,
+          start,
+          end
+        })
+          .catch(err => {
+            throw err
+          })
+      }
+    }
+  })
+  .catch(err => {
+    throw err
+  })
