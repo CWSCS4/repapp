0bcf479e0d06e93d0e71018444c6312e15ac1654
student <student@2B2-iMac.local>
2017-04-25 08:55:00 -0400

    Links table fully functional

--- frontend/LinksView.vue
+++ frontend/LinksView.vue
@@ -24,39 +24,92 @@
               {{ link.repName }}
             </span>
           </md-table-cell>
-          <md-table-cell></md-table-cell>
-          <md-table-cell></md-table-cell>
-          <md-table-cell></md-table-cell>
+          <md-table-cell>
+            <a v-bind:href="link.url">
+              {{ link.uuid }}
+            </a>
+          </md-table-cell>
+          <md-table-cell>
+            <span v-if="link.lastSignedIn">
+              Last signed in on
+              {{ link.lastSignedIn.getMonth() + 1 }}/{{ link.lastSignedIn.getDate() }}
+            </span>
+            <span v-else>
+              Never logged in
+            </span>
+          </md-table-cell>
+          <md-table-cell @click.native="openNotes(link)">
+            <span :class="link.notesFromCollegeSeen ? null : 'unseen'">
+              {{ link.shortenedNotes }}
+            </span>
+          </md-table-cell>
         </md-table-row>
       </md-table-body>
     </md-table>
+
+    <md-dialog-alert :md-content='selectedNotes' md-ok-text="Close" ref="notesDialog">
+    </md-dialog-alert>
   </div>
 </template>
 
 <script>
+  const MAX_NOTES_LENGTH = 40
+  const ELLIPSIS = '...'
+  class Link {
+    constructor({college, repName, uuid, lastSignedIn, notesFromCollege, notesFromCollegeSeen}) {
+      this.college = college
+      this.repName = repName
+      this.uuid = uuid
+      this.lastSignedIn = lastSignedIn
+      this.notesFromCollege = notesFromCollege
+      this.notesFromCollegeSeen = notesFromCollegeSeen
+    }
+    get url() {
+      return '/' + this.uuid
+    }
+    get shortenedNotes() {
+      const notes = this.notesFromCollege
+      if (notes.length > MAX_NOTES_LENGTH) {
+        return notes.substring(0, MAX_NOTES_LENGTH - ELLIPSIS.length) + ELLIPSIS
+      }
+      else return notes
+    }
+  }
   export default {
     name: 'links-view',
     data() {
       return {
         links: [
-          {
+          new Link({
             college: 'Swarthmore',
-            repName: null,
+            repName: 'Josh Throckmorton',
             uuid: 'abcdef',
             lastSignedIn: null,
-            notesFromCollege: null,
+            notesFromCollege: 'Long notes string, hoping this gets cut off',
             notesFromCollegeSeen: true
-          },
-          {
+          }),
+          new Link({
             college: 'UMass Amherst',
-            repName: 'Josh Throckmorton',
+            repName: null,
             uuid: '123456',
             lastSignedIn: new Date,
             notesFromCollege: 'Blah blah blah',
             notesFromCollegeSeen: false
-          }
-        ]
+          })
+        ],
+        selectedNotes: "Click on a college's notes" //should never be shown to the user
+      }
+    },
+    methods: {
+      openNotes(link) {
+        this.selectedNotes = link.notesFromCollege
+        this.$refs.notesDialog.open()
       }
     }
   }
 </script>
+
+<style lang="sass" scoped>
+  span.unseen
+    background: yellow
+</style>
