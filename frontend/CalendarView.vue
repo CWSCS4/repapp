9116a035c59cdf8ea341a5b27d07dd2dfa8ccdfc
student <student@2B7.local>
2017-04-26 11:05:35 -0400

    Work on calendar grid

--- frontend/CalendarView.vue
+++ frontend/CalendarView.vue
@@ -4,8 +4,23 @@
       <md-layout v-for="day in days">
         <md-layout md-column>
           <md-layout md-align="center">
-            <md-layout md-flex="50">
+            <md-layout>
               <b class="day">{{ day.name }}</b>
+            </md-layout>
+            <md-layout v-for="period in day.periods" md-flex="100" class="period">
+              <md-layout md-column>
+                <md-layout class="period-times">
+                  {{ (period.time[0].getHours() + 11) % 12 + 1 }}
+                  :
+                  <span v-if="period.time[0].getMinutes() < 10">0</span>
+                  {{ period.time[0].getMinutes() }}
+                </md-layout>
+                <md-layout class="name">
+                  <div style="margin:auto">
+                    {{ period.period }}
+                  </div>
+                </md-layout>
+              </md-layout>
             </md-layout>
           </md-layout>
         </md-layout>
@@ -26,58 +41,63 @@
         days: [
           {
             name: 'Monday',
-            periods: new Map()
-              .set('1', [makeTime(8, 30), makeTime(9, 10)])
-              .set('2', [makeTime(9, 15), makeTime(9, 55)])
-              .set('3', [makeTime(10, 25), makeTime(11, 5)])
-              .set('4', [makeTime(11, 10), makeTime(11, 50)])
-              .set('5', [makeTime(11, 55), makeTime(12, 35)])
-              .set('6', [makeTime(1, 20), makeTime(2, 0)])
-              .set('7', [makeTime(2, 5), makeTime(2, 45)])
+            periods: [
+              {period: '1', time: [makeTime(8, 30), makeTime(9, 10)]},
+              {period: '2', time: [makeTime(9, 15), makeTime(9, 55)]},
+              {period: '3', time: [makeTime(10, 25), makeTime(11, 5)]},
+              {period: '4', time: [makeTime(11, 10), makeTime(11, 50)]},
+              {period: '5', time: [makeTime(11, 55), makeTime(12, 35)]},
+              {period: '6', time: [makeTime(1, 20), makeTime(2, 0)]},
+              {period: '7', time: [makeTime(2, 5), makeTime(2, 45)]}
+            ]
           },
           {
             name: 'Tuesday',
-            periods: new Map()
-              .set('1', [makeTime(8, 30), makeTime(9, 10)])
-              .set('2', [makeTime(9, 15), makeTime(9, 55)])
-              .set('3', [makeTime(10, 35), makeTime(11, 30)])
-              .set('4', [makeTime(11, 35), makeTime(12, 15)])
-              .set('5', [makeTime(12, 20), makeTime(1, 0)])
-              .set('6', [makeTime(1, 45), makeTime(2, 25)])
-              .set('7', [makeTime(2, 30), makeTime(3, 10)])
+            periods: [
+              {period: '1', time: [makeTime(8, 30), makeTime(9, 10)]},
+              {period: '2', time: [makeTime(9, 15), makeTime(9, 55)]},
+              {period: '3', time: [makeTime(10, 35), makeTime(11, 30)]},
+              {period: '4', time: [makeTime(11, 35), makeTime(12, 15)]},
+              {period: '5', time: [makeTime(12, 20), makeTime(1, 0)]},
+              {period: '6', time: [makeTime(1, 45), makeTime(2, 25)]},
+              {period: '7', time: [makeTime(2, 30), makeTime(3, 10)]}
+            ]
           },
           {
             name: 'Wednesday',
-            periods: new Map()
-              .set('1', [makeTime(8, 30), makeTime(9, 10)])
-              .set('2', [makeTime(9, 15), makeTime(9, 55)])
-              .set('3', [makeTime(10, 25), makeTime(11, 5)])
-              .set('4', [makeTime(11, 10), makeTime(11, 50)])
-              .set('5', [makeTime(11, 55), makeTime(12, 35)])
-              .set('6', [makeTime(1, 20), makeTime(2, 0)])
-              .set('7', [makeTime(2, 5), makeTime(2, 45)])
+            periods: [
+              {period: '1', time: [makeTime(8, 30), makeTime(9, 10)]},
+              {period: '2', time: [makeTime(9, 15), makeTime(9, 55)]},
+              {period: '3', time: [makeTime(10, 25), makeTime(11, 5)]},
+              {period: '4', time: [makeTime(11, 10), makeTime(11, 50)]},
+              {period: '5', time: [makeTime(11, 55), makeTime(12, 35)]},
+              {period: '6', time: [makeTime(1, 20), makeTime(2, 0)]},
+              {period: '7', time: [makeTime(2, 5), makeTime(2, 45)]}
+            ]
           },
           {
             name: 'Thursday',
-            periods: new Map()
-              .set('1', [makeTime(8, 30), makeTime(9, 10)])
-              .set('2', [makeTime(9, 15), makeTime(9, 55)])
-              .set('3', [makeTime(10, 25), makeTime(11, 15)])
-              .set('4', [makeTime(11, 20), makeTime(12, 0)])
-              .set('5', [makeTime(1, 55), makeTime(2, 35)])
-              .set('6', [makeTime(2, 40), makeTime(3, 20)])
-              .set('7', [makeTime(3, 25), makeTime(4, 5)])
+            periods: [
+              {period: '1', time: [makeTime(8, 30), makeTime(9, 10)]},
+              {period: '2', time: [makeTime(9, 15), makeTime(9, 55)]},
+              {period: '3', time: [makeTime(10, 25), makeTime(11, 15)]},
+              {period: '4', time: [makeTime(11, 20), makeTime(12, 0)]},
+              {period: '5', time: [makeTime(1, 55), makeTime(2, 35)]},
+              {period: '6', time: [makeTime(2, 40), makeTime(3, 20)]},
+              {period: '7', time: [makeTime(3, 25), makeTime(4, 5)]}
+            ]
           },
           {
             name: 'Friday',
-            periods: new Map()
-              .set('1', [makeTime(8, 30), makeTime(9, 10)])
-              .set('2', [makeTime(9, 15), makeTime(9, 55)])
-              .set('3', [makeTime(10, 25), makeTime(11, 5)])
-              .set('4', [makeTime(11, 10), makeTime(11, 50)])
-              .set('5', [makeTime(11, 55), makeTime(12, 35)])
-              .set('6', [makeTime(1, 20), makeTime(2, 0)])
-              .set('7', [makeTime(2, 5), makeTime(2, 45)])
+            periods: [
+              {period: '1', time: [makeTime(8, 30), makeTime(9, 10)]},
+              {period: '2', time: [makeTime(9, 15), makeTime(9, 55)]},
+              {period: '3', time: [makeTime(10, 25), makeTime(11, 5)]},
+              {period: '4', time: [makeTime(11, 10), makeTime(11, 50)]},
+              {period: '5', time: [makeTime(11, 55), makeTime(12, 35)]},
+              {period: '6', time: [makeTime(1, 20), makeTime(2, 0)]},
+              {period: '7', time: [makeTime(2, 5), makeTime(2, 45)]}
+            ]
           }
         ]
       }
