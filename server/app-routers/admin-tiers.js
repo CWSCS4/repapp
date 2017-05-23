81df1c5d741bec87988961e8191bd0024218e13c
Randy <randyzhou1999@gmail.com>
2017-05-22 22:17:28 -0400

    Added respondWithError to replace catch error blocks.

--- server/app-routers/admin-tiers.js
+++ server/app-routers/admin-tiers.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const db = require('../database')
-
+const respondWithError = require('../respond-with-error')
 const router = express.Router()
 
 router.get('/', function (req, res){
   db.tier.findAll({attributes: ['priority','description']}).then(tiers => {
       res.json({success: true, tiers: tiers})
-  }).catch(function (err){
-    res.json({success: false, message: err.message})
-  })
+  }).catch(respondWithError(res))
 })
 
 router.post('/', function (req, res){
@@ -17,17 +15,13 @@ router.post('/', function (req, res){
     description:req.body.description
 	}).then ( tier => {
 		res.json({success : true})
-	}).catch(function (err){
-      res.json({success: false, message: err.message})
-  	})
+	}).catch(respondWithError(res))
 })
 
 router.delete('/:priority', function (req, res) {
   db.tier.destroy({where: {priority:req.params.priority} }).then(tier => {
       res.json({success: true})
-  }).catch(function (err){
-    res.json({success: false, message: err.message})
-  })
+  }).catch(respondWithError(res))
 })
 
 module.exports = router
